576da3c4cce5393b9385ca7d2fa884ba8bea9d7c
Okwuchukwu Nwobi <dnwobi1@gmail.com>
2025-02-24 08:35:13 +0000

    Integrated far singularity terms

--- include/infMat.hpp
+++ include/infMat.hpp
@@ -7,21 +7,59 @@
 #include <vector>
 namespace views = std::views;
 #define RANGE(n) views::iota(0, (int)n)
-
+typedef Array<bool, Dynamic, 1> ArrayXb;
 template <typename Singularity, bool SelfInfluence> // concept constrain
-Eigen::ArrayXXd
-makeInfluenceMatrix(int m, int n, const std::vector<ComputeTask> &compTaskVec) {
+Eigen::ArrayXXd makeInfluenceMatrix(int m, int n,
+                                    std::vector<ComputeTask> &compTaskVec) {
 #if (BENCHMARKING == 0)
   print(__PRETTY_FUNCTION__);
 #endif
   Eigen::ArrayXXd infMat(m, n);
   infMat.setZero();
+
+  std::vector<std::size_t> partioned_indices(compTaskVec[0].points.rows());
+  std::iota(partioned_indices.begin(), partioned_indices.end(), 0);
+  Eigen::ArrayXd norms(compTaskVec[0].points.rows());
+  Eigen::ArrayXi isFarAway(norms.size());
+  Eigen::ArrayXd solution;
+  ComputeTask temp;
+  temp.points.conservativeResizeLike(compTaskVec[0].points);
+
+  double limit = 10;
   for (auto i : RANGE(compTaskVec.size())) {
+    const auto &face = compTaskVec[i].face;
 
-    infMat(compTaskVec[i].indices, i) =
-        Singularity::calcInfluence(compTaskVec[i]);
-    ;
+    // Reference diameter
+    double maxDiameter =
+        (face.points.row(0) - face.points.row(2)).matrix().norm();
+    maxDiameter = std::max(
+        maxDiameter, (face.points.row(0) - face.points.row(2)).matrix().norm());
+    // split into near and far point
+    norms = compTaskVec[i].points.matrix().rowwise().norm();
+    isFarAway = (norms < limit * maxDiameter) // Distance Condition
+                    .select(Eigen::ArrayXi::Ones(norms.size()),
+                            Eigen::ArrayXi::Zero(norms.size()));
 
+    auto splitLoc = std::partition(
+        partioned_indices.begin(), partioned_indices.end(),
+        [&isFarAway](std::size_t idx) { return isFarAway[idx]; });
+
+    temp.face = compTaskVec[i].face;
+
+    // update tempTask with far points;
+    temp.points = compTaskVec[i].points(
+        std::span<std::size_t>(partioned_indices.begin(), splitLoc),
+        Eigen::placeholders::all);
+    infMat(std::span<std::size_t>(partioned_indices.begin(), splitLoc), i) =
+        Singularity::calcInfluence(temp);
+
+    // update tempTask with far points;
+    temp.points = compTaskVec[i].points(
+        std::span<std::size_t>(splitLoc, partioned_indices.end()),
+        Eigen::placeholders::all);
+    infMat(std::span<std::size_t>(splitLoc, partioned_indices.end()), i) =
+        Singularity::calcInfluenceFar(temp);
+    // is this allocationg new memory
     if constexpr (SelfInfluence) {
       ComputeTask temp;
       temp.face = compTaskVec[i].face;
@@ -29,6 +67,7 @@ makeInfluenceMatrix(int m, int n, const std::vector<ComputeTask> &compTaskVec) {
       temp.points = (Eigen::ArrayX3d(1, 3) << 0, 0, 0).finished();
       infMat(i, i) = Singularity::calcSelfInfluence(temp);
     }
+    std::iota(partioned_indices.begin(), partioned_indices.end(), 0);
   }
   // print(infMat.topLeftCorner(10, 10));
 #if (BENCHMARKING == 0)
