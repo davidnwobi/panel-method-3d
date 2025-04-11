7fc7bc4253acd0ae6a66c3661e08107cab6520e1
Okwuchukwu Nwobi <dnwobi1@gmail.com>
2025-04-11 22:39:55 +0100

    Coalesed R12 Q12 J12

--- src/singularity/internal_functions.cpp
+++ src/singularity/internal_functions.cpp
@@ -119,6 +119,68 @@ Eigen::ArrayXd J12(const Eigen::Ref<const Eigen::ArrayX3d> &points,
   return out;
 }
 
+double slope(double xA, double yA, double xB, double yB) {
+  const double dx = xB - xA;
+  const double dy = yB - yA;
+  // handle near‐vertical or near‐horizontal
+  if (std::fabs(dx) < 1e-14) {
+    return sgn(dy) * std::numeric_limits<double>::infinity();
+  }
+  if (std::fabs(dy) < 1e-14) {
+    return 0.0;
+  }
+  return dy / dx;
+};
+void R12_Q12_J12(Eigen::Ref<Eigen::ArrayXd> &R12_,
+                 Eigen::Ref<Eigen::ArrayXd> &Q12_,
+                 Eigen::Ref<Eigen::ArrayXd> &J12_,
+                 const Eigen::Ref<const Eigen::ArrayX3d> &points,
+                 const Eigen::Ref<const Eigen::Array3d> &node1,
+                 const Eigen::Ref<const Eigen::Array3d> &node2) {
+
+  const double x1 = node1(0), y1 = node1(1);
+  const double x2 = node2(0), y2 = node2(1);
+  const double m12 = slope(x1, y1, x2, y2);
+
+  const double dx = node2(0) - node1(0);
+  const double dy = node2(1) - node1(1);
+  const double d = std::sqrt(dx * dx + dy * dy);
+
+  for (Eigen::Index i = 0; i < points.rows(); ++i) {
+    const double px = points(i, 0);
+    const double py = points(i, 1);
+    const double pz = points(i, 2);
+
+    R12_[i] = ((px - node1(0)) * dy - (py - node1(1)) * dx) / d;
+
+    // For node1:
+    const double dx1 = px - x1;
+    const double dy1 = py - y1;
+    const double e1 = dx1 * dx1 + pz * pz; // ek(node1)
+    const double h1 = dx1 * dy1;           // hk(node1)
+    const double r1 = std::sqrt(dx1 * dx1 + dy1 * dy1 + (pz * pz));
+
+    // For node2:
+    const double dx2 = px - x2;
+    const double dy2 = py - y2;
+    const double e2 = dx2 * dx2 + pz * pz; // ek(node2)
+    const double h2 = dx2 * dy2;           // hk(node2)
+    const double r2 = std::sqrt(dx2 * dx2 + dy2 * dy2 + (pz * pz));
+
+    Q12_[i] = std::log((r1 + r2 + d) / (r1 + r2 - d));
+
+    // termP(m, e, h, r) = atan( (m*e - h) / (pz*r) )
+    // watch for pz=0.0?
+    auto termP = [&](double m, double e, double h, double rr) {
+      // if pz=0, you might want to handle that carefully
+      const double denom = pz * rr;
+      return std::atan((m * e - h) / denom);
+    };
+
+    // difference of termP for node1, node2
+    J12_[i] = termP(m12, e1, h1, r1) - termP(m12, e2, h2, r2);
+  }
+}
 // Eigen::ArrayXd J12_OLD(const Eigen::Ref<const Eigen::ArrayX3d> &points,
 //                        const Eigen::Ref<const Eigen::Array3d> &node1,
 //                        const Eigen::Ref<const Eigen::Array3d> &node2) {
